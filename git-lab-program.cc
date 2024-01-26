c83977799391349a69aa427ef81eea771b348de8
adozmati <157167551+adozmati@users.noreply.github.com>
2024-01-26 15:58:18 -0500

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+# git-lab-2
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Alex Dozmati>
+ *        Date: <Jan. 26 2024>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    return 0;
+}// main
+cout << "Hello Git!" << endl;
